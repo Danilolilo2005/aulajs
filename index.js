93a02755a4e6eafe4f0140dc2405e4e0a30dee01
Daniel Rodriguez <108771601+Danilolilo2005@users.noreply.github.com>
2023-08-15 11:06:53 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+function novo(){
+    var nome = document.formizinho.nome.value
+    var rua = document.formizinho.rua.value
+    {
+        document.write("A Correios Av Brg Fari<br>")
+        document.write("Nome:"+ nome +"<br>")
+        document.write("Rua:"+ rua +"<br>")
+        document.write("Atenciosamente,<br><br>")
+        document.write("Júlio Batista<br>")
+        document.write("Presidente da ABC/SUZ Trainning Corporation<br>")
+        document.write("Treinamento com qualidade com custo reduzido")
+    }
+}
